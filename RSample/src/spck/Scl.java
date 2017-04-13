ce0b93929a062e80a1dea73af83d72513abd231b
RM00363966 <RM00363966@techMahindra.com>
2017-04-13 13:13:35 +0530

    1st checkin

---
+++ RSample/src/spck/Scl.java
@@ -0,0 +1,14 @@
+package spck;
+
+public class Scl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1st checkin");
+
+	}
+
+}
